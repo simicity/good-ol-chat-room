650140a67851597092729b5021f0c960788b8969
simicity <simi40nm@gmail.com>
2023-06-01 22:32:55 -0700

    Show message timestamp

--- client/src/components/chat/ChatMessage.tsx
+++ client/src/components/chat/ChatMessage.tsx
@@ -32,18 +32,53 @@ const generateRandomColor = (username: string) => {
   return color;
 }
 
+const createTimeStamp = (date: Date) => {
+  const timestamp = new Date(date);
+  const options: Intl.DateTimeFormatOptions = {
+    timeZone: 'America/Los_Angeles',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: 'numeric',
+    minute: '2-digit',
+    second: '2-digit'
+  };
+
+  return timestamp.toLocaleString('en-US', options);
+}
+
 function ChatMessage({ message }: { message: messageData }) {
 
   return (
-    <Box sx={style}>
-      <Typography variant="caption" component="p" sx={{ color: generateRandomColor(message.from), fontWeight: "bold" }}>
-        {message.from}
-      </Typography>
-      <Typography variant="body2" component="p">
-        {message.message}
-      </Typography>
-    </Box>
-  )
+    <>
+      {message.from ? (
+        <Box sx={style}>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Typography variant="caption" component="span" sx={{ color: generateRandomColor(message.from), fontWeight: "bold" }}>
+              {message.from}
+            </Typography>
+            <Typography variant="caption" component="span">
+              {createTimeStamp(message.timestamp)}
+            </Typography>
+          </Box>
+          <Typography variant="body2" component="p">
+            {message.message}
+          </Typography>
+        </Box>
+      ) : (
+        <Box sx={style}>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Typography variant="body2" component="p">
+              {message.message}
+            </Typography>
+            <Typography variant="caption" component="span">
+              {createTimeStamp(message.timestamp)}
+            </Typography>
+          </Box>
+        </Box>
+      )}
+    </>
+  );
 }
 
 export default ChatMessage;
