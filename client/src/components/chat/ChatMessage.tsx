86ad3f5fcb6cdba76e1ea1d65c038eb022ff5551
simicity <simi40nm@gmail.com>
2023-07-05 16:23:25 -0700

    Fix timestamp format in chat messages

--- client/src/components/chat/ChatMessage.tsx
+++ client/src/components/chat/ChatMessage.tsx
@@ -39,12 +39,12 @@ const createTimeStamp = (date: Date) => {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
-    hour: 'numeric',
+    hour: '2-digit',
     minute: '2-digit',
     second: '2-digit'
   };
 
-  return timestamp.toLocaleString('en-US', options);
+  return timestamp.toLocaleString('en-US', options).replace(",", "");
 }
 
 function ChatMessage({ message }: { message: messageData }) {
